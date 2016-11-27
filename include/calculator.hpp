85d0f3bb7238bf1b9673ee87a2d3c479607ef827
Zarimiro <vadimzarya@yandex.ru>
2016-11-27 22:41:27 +0400

    Create calculator.hpp

---
+++ include/calculator.hpp
@@ -0,0 +1,7 @@
+double sum(int a, int b);
+double dev(int a, int b);
+double mult(int a, int b);
+double sub(int a, int b);
+unsigned int pow(int a, int b);
+double sqrta(int a);
+double sqrtb(int b);
